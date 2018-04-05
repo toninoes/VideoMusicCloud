11bfdf93100fe15d1bb24b3f685dc29b74ff1703
Antonio Ruiz Rondán <toninoes@gmail.com>
2018-04-05 19:19:45 +0200

    Explicación acerca de las posibles ubicaciones de los vídeos

--- src/main/java/vmc/VideoLocationProperties.java
+++ src/main/java/vmc/VideoLocationProperties.java
@@ -6,10 +6,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class VideoLocationProperties {
 
     /**
-     * Localización de los vídeos
+     * Localización de los vídeos, puede utilizarse ruta absoluta o relativa
+     * como pueden verse en las siguientes líneas comentadas, si el directorio
+     * 'videos' no existe, lo crea
      */
+	
+	// ubicación dentro del mismo proyecto. en una ubicación concreta
     //private String location = "src/main/resources/static/videos";
-	private String location = "../videos";
+	
+	// fuera del proyecto en una ruta absoluta de un servidor Gnu/Linux
+	//private String location = "/home/toni/videos";
+	
+	//o en una partición concreta de un equipo Windows.
+	private String location = "D:\\videos";
 
     public String getLocation() {
         return location;
