1d804c653994b9998f56ba4338b84c8b8f3e1be8
LuisferPP <luisfer_pp@hotmail.com>
2018-04-11 12:29:41 +0200

    Creada clase Comentario

---
+++ src/main/java/vmc/model/Comentario.java
@@ -0,0 +1,57 @@
+package vmc.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "comentarios")
+public class Comentario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@NotBlank(message = "*Introduzca un texto para el comentario")
+	private String descripcion;
+	
+	@ManyToOne
+	@JoinColumn(name="usuario_id", nullable = false)
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JoinColumn(name="video_id", nullable = false)
+	private Video video;
+	
+	@Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date creacion;
+	
+	public Comentario() {
+		super();
+	}
+	
+	public Comentario(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getDescripcion() {return this.descripcion;}
+	public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+
+}
