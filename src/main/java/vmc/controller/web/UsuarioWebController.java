cd6fbc69074f7c85a0ad2398a6d0f4abefc40afd
chemari31 <josemaria.garciasanchez@alum.uca.es>
2018-04-15 13:48:49 +0200

    Vista usuario y inicio de 2 en 2 videos

--- src/main/java/vmc/controller/web/UsuarioWebController.java
+++ src/main/java/vmc/controller/web/UsuarioWebController.java
@@ -1,8 +1,10 @@
 package vmc.controller.web;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
