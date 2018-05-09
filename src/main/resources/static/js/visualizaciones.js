2222e6a1b1129347c77481f5c7cc75edd99510d7
AndrewM-G <abreu1981.am@gmail.com>
2018-05-09 19:31:02 +0200

    Commit presentación del miércoles "la previa"

--- src/main/resources/static/js/visualizaciones.js
+++ src/main/resources/static/js/visualizaciones.js
@@ -1,29 +1,17 @@
 function visitsComentario() {
-	if(document.getElementById("visitComentario") == null)
-		document.getElementById("visitComentario").innerText =  1;
-	else	
-		document.getElementById("visitComentario").innerText =  1 + parseInt(document.getElementById("visitComentario").innerText);
+	document.getElementById("visitComentario").innerText =  1 + parseInt(document.getElementById("visitComentario").innerText);
 }
 
 function visitsPerfil() {
-	if(document.getElementById("visitPerfil") == null)
-		document.getElementById("visitPerfil").innerText =  1;
-	else
-		document.getElementById("visitPerfil").innerText =  1 + parseInt(document.getElementById("visitPerfil").innerText);
+	document.getElementById("visitPerfil").innerText =  1 + parseInt(document.getElementById("visitPerfil").innerText);
 }
 
 function visitsInicio() {
-	if(document.getElementById("visitInicio") == null)
-		document.getElementById("visitInicio").innerText =  1;
-	else
-		document.getElementById("visitInicio").innerText =  1 + parseInt(document.getElementById("visitInicio").innerText);
+	document.getElementById("visitInicio").innerText =  1 + parseInt(document.getElementById("visitInicio").innerText);
 }
 
 function visitsListas() {
-	if(document.getElementById("visitListas") == null)
-		document.getElementById("visitListas").innerText =  1;
-	else
-		document.getElementById("visitListas").innerText =  1 + parseInt(document.getElementById("visitListas").innerText);
+	document.getElementById("visitListas").innerText =  1 + parseInt(document.getElementById("visitListas").innerText);
 }
 
 function viewsComentario(nodo) {
