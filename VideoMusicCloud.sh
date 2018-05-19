3e32525dc5df895415bafd4f4ec5c341251c1e29
Antonio Ruiz Rondán <toninoes@gmail.com>
2018-05-19 14:25:56 +0200

    Actualizados scripts

--- VideoMusicCloud.sh
+++ VideoMusicCloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "[INFO] Creando base de datos para VideoMusicCloud...\n"
-printf "[MySQL] Introduzca su contraseña de root de MySQL\n"
+printf "[MySQL] Introduzca su password de root de MySQL\n"
 
 if mysql -u root -p < VideoMusicCloud.sql; then
   printf "[OK] Base de datos creada correctamente\n"
@@ -9,9 +9,10 @@ else
   printf "[ERROR] No se ha podido crear la base de datos\n"
 fi
 
-printf "[INFO] Dato necesario para el despliegue de VideoMusicCloud...\n"
-printf "[Tomcat] Indique directorio de destino de Tomcat para ubicar fichero .war\n"
-printf "[Tomcat] Pulse enter si quiere que sea [/var/lib/tomcat8/webapps]: "
+printf "[INFO] Necesario para el despliegue de VideoMusicCloud...\n"
+printf "[Tomcat] Indique directorio de Tomcat.\n"
+printf "[Tomcat] Por defecto [/var/lib/tomcat8/webapps]:\n"
+printf "[Tomcat] Pulsa ENTER para dejarlo por defecto."
 
 read dir
 
@@ -19,17 +20,18 @@ if [ "$dir" == "" ]; then
   dir="/var/lib/tomcat8/webapps"
 fi
 
-printf "[INFO] Desplegando la aplicación. Espere......\n"
+printf "[INFO] Desplegando la aplicacion. Espere......\n"
 if mvn clean install -Ddir=$dir; then
-  printf "[OK] Aplicación desplegada correctamente\n"
+  printf "[OK] Desplegada correctamente\n"
 else
-  printf "[ERROR] Ha habido un problema al desplegar la aplicación.\n"
+  printf "[ERROR] Ha habido un problema en el despliegue.\n"
 fi
 
 printf "[INFO] Reiniciando Tomcat. Espere......\n"
 if service tomcat8 restart; then
   sleep 25
-  printf "[OK] Tomcat reiniciado, vaya a: http://localhost:8080/VideoMusicCloud\n"
+  printf "[OK] Tomcat reiniciado\n"
+  printf "[OK] Vaya a: http://localhost:8080/VideoMusicCloud\n"
 else
   printf "[ERROR] Ha habido un problema al reiniciar Tomcat.\n"
 fi
--- VideoMusicCloud.sql
+++ VideoMusicCloud.sql
@@ -4,8 +4,9 @@
 CREATE DATABASE IF NOT EXISTS `videomusiccloud` DEFAULT CHARACTER SET utf8;
 
 --
--- Damos todos los privilegios al usuario: VideoMusicCloud con clave: VideoMusicCloud sobre BBDD: videomusiccloud
--- Si el usuario no existe lo creará
+-- Damos todos los privilegios al usuario: VideoMusicCloud 
+-- con clave: VideoMusicCloud sobre BBDD: videomusiccloud
+-- Si el usuario no existe lo crea
 --
 GRANT ALL ON `videomusiccloud`.* TO 'VideoMusicCloud'@'localhost' IDENTIFIED BY 'VideoMusicCloud';
 
