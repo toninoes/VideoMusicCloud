3566bd84b28a3c75ebda9cda229c892db43d4a72
AndrewM-G <abreu1981.am@gmail.com>
2018-05-30 10:31:19 +0200

    Commit correción del error del test de subida de video en la selección del género

--- src/test/java/selenium/SeleniumTests.java
+++ src/test/java/selenium/SeleniumTests.java
@@ -3,7 +3,9 @@ package selenium;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTests {
 		
@@ -13,7 +15,7 @@ public class SeleniumTests {
 	{
 		// Cambiar por directorio donde tengas instalado geckodriver
 		// O bien ponerlo en el PATH
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Santiago\\Documents\\PINF\\geckodriver-v0.18.0-win64\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\abreu\\Documents\\PINF\\geckodriver-v0.18.0-win64\\geckodriver.exe");
 		this.driver = new FirefoxDriver();		
 	}
 	
@@ -22,9 +24,9 @@ public class SeleniumTests {
 		SeleniumTests test = new SeleniumTests();
 		//test.TestRegistrarUsuario();
 		//test.TestLogin();
-		//test.TestSubirVideo();
+		test.TestSubirVideo();
 		//test.TestBuscarVideo();
-		test.TestBuscarVideoFiltrado();
+		//test.TestBuscarVideoFiltrado();
 	}
 	
 	public void TestRegistrarUsuario()
@@ -51,10 +53,13 @@ public class SeleniumTests {
 		TestLogin();
 		driver.findElement(By.linkText("Subir Video")).click();
 		// Cambiar la ruta por la del video que quieras subar
-		driver.findElement(By.id("videobox")).sendKeys("C:\\Users\\Santiago\\Videos\\allend\\jackiechan.mp4");
+		driver.findElement(By.id("videobox")).sendKeys("C:\\Users\\abreu\\Videos\\allend\\1_68.mp4");
 		driver.findElement(By.id("titulo")).sendKeys("Titulo de Prueba Selenium");
-		driver.findElement(By.id("descripcion")).sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue eu elit eu tincidunt. Nullam ut lectus at tellus convallis varius. Pellentesque aliquam mauris est, quis blandit diam dignissim in. Etiam nec urna in eros aliquam euismod. Nullam nec rutrum erat. Donec tristique posuere mauris. Ut vitae lacus at diam aliquet dictum quis ac sapien.");
+		driver.findElement(By.id("descripcion")).sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue eu elit eu tincidunt. Nullam ut lectus at tellus convallis varius.");
 		Select oSelect = new Select(driver.findElement(By.id("videogeneros")));
+		WebDriverWait wait = new WebDriverWait(driver, 7);
+		WebElement spinner = driver.findElement(By.xpath("//div[@class='be-loader']"));
+		wait.until(ExpectedConditions.invisibilityOf(spinner));
 		oSelect.selectByVisibleText("pop");
 		driver.findElement(By.id("subirvideo")).submit();
 	}
